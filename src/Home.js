99beafaed6b1ed74b124b1a39b9ea9269516c731
Nitin96Bisht <nitin.bisht96@gmail.com>
2020-09-06 19:53:12 +0530

    Added banner

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Home.css';
+import Banner from './Banner.js';
 
 function Home() {
     return (
         <div className="home">
-
+            <Banner />
         </div>
     )
 }
