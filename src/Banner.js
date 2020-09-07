7ed218a295d1547590ced81c7ac96f2d0b378701
Nitin96Bisht <nitin.bisht96@gmail.com>
2020-09-07 23:05:27 +0530

    Added search date range

--- src/Banner.js
+++ src/Banner.js
@@ -11,7 +11,8 @@ function Banner() {
                 {showSearch && <Search />}
                 <Button onClick={() => setShowSearch(!showSearch)}
                     className="banner__searchButton"
-                    variant="outlined">Search Dates
+                    variant="outlined">
+                    {showSearch ? "Hide" : "Search Dates"}
                 </Button>
             </div>
             <div className="banner__info">
