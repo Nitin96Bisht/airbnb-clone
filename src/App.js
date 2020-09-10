1923fbb926c55bd2281c1fe6816e18bebaaed77b
Nitin96Bisht <nitin.bisht96@gmail.com>
2020-09-10 22:37:27 +0530

    Added footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Home from './Home.js';
 import Header from './Header.js';
+import Footer from './Footer.js';
 
 function App() {
   return (
     <div className="app">
       <Header />
       <Home />
+      <Footer />
     </div>
   );
 }
